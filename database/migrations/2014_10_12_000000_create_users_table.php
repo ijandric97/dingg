de5f84bbecd72f97b5245ac91b7874219f180335
Ivan Jandric <ijandric@iolap.com>
2020-07-13 23:42:05 +0200

    Changed user model and added seeder

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,9 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->string('role');
+            $table->string('phone');
+            $table->string('image_path');
         });
     }
 
--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -2,8 +2,12 @@
 
 use Illuminate\Database\Seeder;
 
+use App\User;
+
 class UserSeeder extends Seeder
 {
+
+    
     /**
      * Run the database seeds.
      *
@@ -11,6 +15,17 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $faker = \Faker\Factory::create(); // FIXME: Remove this and get proper pictures :)
+
+        User::truncate();
+
+        $users = [
+            ['name' => 'food_city_hamburger', 'email' => 'food_city_hamburger@test.com', 'password' => bcrypt('12345678'),
+                'role' => 'restaurant', 'phone' => '+38551330653', 'image_path' => $faker->image(storage_path('app\public\images'), 256, 256, null, false)],
+        ];
+
+        foreach ($users as $user) {
+            User::create($user);
+        }
     }
 }
